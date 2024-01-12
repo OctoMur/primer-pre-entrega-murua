14bbba758c209f01a6d8ab9edceb4b93d07f656b
octavio <octavioAISM@gmail.com>
2024-01-12 09:36:59 -0300

    Implementacion de metodo GET y POST en products

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 const express = require("express");
-const app = express;
+const app = express();
 const PORT = 8080;
 
 const productsRouter = require("./routes/products.router");
 
 //middleware
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
 
+//routes
 app.use("/api", productsRouter);
 
 app.listen(PORT, ()=>{
--- src/controllers/productManager.js
+++ src/controllers/productManager.js
@@ -2,21 +2,45 @@ const fs = require("fs").promises;
 
 
 class ProductManager{
-    static idProduct = 1;
+    static idProduct = 0;
 
     constructor(productsDbPath){
-        this.products = [];
+        this.products = []; //actualizar mas adelante para que verifique si no existe un archivo JSON con productos guardados.
         this.path = productsDbPath;
+        this.verifyProducts();
     }
+
+    async verifyProducts() {
+        try {
+            const productsList = await this.readFile();
+    
+            if (productsList && productsList.length > 0) {
+                this.products = productsList;
+    
+                // Usar reduce con un valor inicial para asegurar la comparación
+                const maxId = productsList.reduce((max, product) => (product.id > max ? product.id : max), 0);
+    
+                if (maxId > ProductManager.idProduct) {
+                    ProductManager.idProduct = maxId;
+                    console.log(ProductManager.idProduct);
+                }
+            } else {
+                console.log('La lista de productos está vacía o no se pudo leer.');
+            }
+        } catch (error) {
+            console.error('Error al verificar productos:', error.message);
+        }
+    }
+
 
     async addProduct(newProduct){
         //destructuracion del objeto
-        let {title, description, price, img, code, stock} = newProduct;
+        let {title, description, code, price, status, stock, category, thumbnails} = newProduct;
 
         //Validaciones
-        if(!title || !description || !price || !img || !code || !stock){
+        if(!title || !description || !code || !price || !status || !stock || !category || !thumbnails){
             console.log("Hay uno o mas campos vacios")
-            return;
+            return;thumbnails
         }
 
         if(this.products.some(item => item.code === code)){
@@ -24,24 +48,37 @@ class ProductManager{
             return;
         }
 
+        const product={
+            id: ++ProductManager.idProduct,
+            title: title,
+            description: description,
+            code: code,
+            price: price,
+            status: status,
+            stock: stock,
+            category: category,
+            thumbnails: thumbnails
+        }
+
         //sumar el producto al array y pushear a la DB
-        this.products.push(newProduct)
-        await this.saveFile(this.products)
+        this.products.push(product);
+        await this.saveFile(this.products);
     }
 
-    getProducts(){
-        return console.log(this.products)
+    async getProducts(){
+        return this.products;
     }
 
     async getProductById(id){
         try {
             const productsImported = await this.readFile();
-            const productFinded = productsImported.find(item => item.id === id);
+            const productFinded = productsImported.find(item => item.id == id);
 
             if(!productFinded){
                 console.log("Producto no encontrado");
             }else{
                 console.log("Producto encotrado!! (☞ﾟヮﾟ)☞ ", productFinded);
+                return productFinded;
             }
         } 
         catch (error) {
@@ -53,6 +90,7 @@ class ProductManager{
         try{
             const res = await fs.readFile(this.path, "utf-8");
             const newArrayObjetcs = JSON.parse(res);
+            this.products = newArrayObjetcs;
             return newArrayObjetcs;
         }catch (error){
             console.log("error al leer el archivo", error);
